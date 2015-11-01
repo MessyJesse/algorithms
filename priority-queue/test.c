f1171fb9ab75ce280481e27c2b8227d2d7629661
Jesse DuMond <jesse.dumond@gmail.com>
2015-11-01 13:54:14 -0500

    Add test for min-heap and max-heap queues

--- priority-queue/test.c
+++ priority-queue/test.c
@@ -8,22 +8,68 @@ compare dummy_func = (void *)0xDEADBEEF;
 
 static int int_min_cmp(const void *obj1, const void *obj2)
 {
+    int val1 = *((int *)obj1);
+    int val2 = *((int *)obj2);
 
+    if (val1 > val2) {
+        return 2;
+    } 
+    
+    if (val1 < val2) {
+        return 1;
+    }
+
+    return 0;
 }
 
 static int int_max_cmp(const void *obj1, const void *obj2)
 {
+    int val1 = *((int *)obj1);
+    int val2 = *((int *)obj2);
 
+    if (val1 > val2) {
+        return 1;
+    } 
+    if (val1 < val2) {
+        return 2;
+    } 
+    
+    return 0;
 }
 
-static char *pq_insert_test()
-{
-    char test_data[10];
+static char *pq_max_test()
+{    
     int i;
-    struct pq *queue = pq_create(dummy_func);
+    int test_data[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    struct pq *queue = pq_create(int_max_cmp);
 
     for (i = 0; i < 10; i++) {
         pq_insert(queue, &test_data[i]);
+    }
+
+    for (i = 0; i < 10; i++) {
+        int val = *((int *)pq_remove(queue));
+        mu_assert("values removed out of order\n", val == test_data[9 - i]);
+    }
+
+    pq_destroy(queue);
+
+    return 0;
+}
+
+static char *pq_min_test()
+{
+    int i;
+    int test_data[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    struct pq *queue = pq_create(int_min_cmp);
+
+    for (i = 0; i < 10; i++) {
+        pq_insert(queue, &test_data[i]);
+    }
+
+    for (i = 0; i < 10; i++) {
+        int val = *((int *)pq_remove(queue));
+        mu_assert("values removed out of order\n", val == test_data[i]);
     }
 
     pq_destroy(queue);
@@ -47,7 +93,8 @@ static char *pq_create_test()
 static char *all_tests() 
 {
     mu_run_test(pq_create_test);
-    mu_run_test(pq_insert_test);
+    mu_run_test(pq_min_test);
+    mu_run_test(pq_max_test);
     return 0;
 }
 
@@ -56,7 +103,7 @@ int main(int argc, char **argv)
     char *result = all_tests();
 
     if (result != 0) {
-        printf("%s\n", result);
+        printf("%s", result);
     } else {
         printf("ALL TESTS PASSED\n");
     }
