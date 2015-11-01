0f7b423a586e002cf6e1c1456f33c31384dce891
Jesse DuMond <jesse.dumond@gmail.com>
2015-11-01 13:26:36 -0500

    Add pq_remove

--- priority-queue/priority_queue.c
+++ priority-queue/priority_queue.c
@@ -14,6 +14,37 @@ static inline unsigned int heap_parent(unsigned int i)
     return i / 2;
 }
 
+static inline unsigned int heap_child(unsigned int i)
+{
+    return i * 2;
+}
+
+static void pq_bubble_down(struct pq *queue, unsigned int parent)
+{
+    int i;
+    int order;
+    unsigned int min_index = parent;
+    unsigned int child = heap_child(parent);
+
+    for (i =  0; i <=  1; i++) {
+        if (child + i > queue->num_obj) {
+            break;
+        }
+
+        order = queue->compare_obj(queue->queue[min_index], queue->queue[child + i]);
+        if (order == 2) {
+            min_index = child + i;
+        }
+    }
+    
+    if (min_index != parent) {
+        void *min_obj = queue->queue[min_index];
+        queue->queue[min_index] = queue->queue[parent];
+        queue->queue[parent] = min_obj;
+        pq_bubble_down(queue, min_index);
+    }
+}
+
 static void pq_bubble_up(struct pq *queue, unsigned int child)
 {
     int order;
@@ -24,7 +55,7 @@ static void pq_bubble_up(struct pq *queue, unsigned int child)
     }    
 
     order = queue->compare_obj(queue->queue[child], queue->queue[parent]);
-    if (order > 0) {
+    if (order == 1) {
         void *child_obj = queue->queue[child];
         queue->queue[child] = queue->queue[parent];
         queue->queue[parent] = child_obj;
@@ -92,6 +123,26 @@ int pq_insert(struct pq *queue, void *obj)
     pq_bubble_up(queue, queue->num_obj);
 
     return 0;
+}
+
+void *pq_remove(struct pq *queue)
+{
+    void *ret;
+
+    if (!queue) {
+        return NULL;
+    }
+
+    if (queue->num_obj == 0) {
+        return NULL;
+    }
+
+    ret = queue->queue[1]; 
+    queue->queue[1] = queue->queue[queue->num_obj];
+    queue->num_obj--;
+    pq_bubble_down(queue, 1);
+
+    return ret;
 }
 
 #if 0
